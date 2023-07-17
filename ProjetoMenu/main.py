2a691146dfec48e32a556aa9b676a47402387406
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2023-07-17 09:55:06 -0300

    Atualização
    implementação de logotipo

--- ProjetoMenu/main.py
+++ ProjetoMenu/main.py
@@ -4,7 +4,8 @@ import random
 
 
 janela = tk.Tk()
-janela.iconbitmap('icone.ico')
+imagem = tk.PhotoImage(file="logo.png") # imagem para Logo
+janela.iconbitmap('icone.ico') # imagem de link
 janela.rowconfigure(0, weight=1)
 janela.columnconfigure([0,1], weight=1)
 
@@ -20,6 +21,9 @@ def mega_sena():
     tema["text"] = lista
 
 
+logotipo = tk.Label(janela, image=imagem)
+logotipo.grid(row=2, column=1, sticky="NSEW")
+
 tema = tk.Label(text="Sorteio da Mega-Sena", fg="#1C1C1C", bg="#aef5c3")
 tema.grid(row=0, column=0, columnspan=2, sticky="NSEW")
 tema.configure(font=("Cooper Black", 20, "bold" ))
@@ -29,7 +33,8 @@ tema.configure(font=("Cooper Black", 20, "bold" ))
 mensagem = tk.Label()
 mensagem.grid(row=3, column=1, columnspan=2, sticky="NSEW")
 
-botao = tk.Button(janela, text="Gerar numeros para Mega-Sena", command=mega_sena, width=80, height=2)
+botao = tk.Button(janela, text="Gerar numeros para Mega-Sena",  command=mega_sena, width=80, height=2)
 botao.grid(row=3, column=1, columnspan=2)
 
 janela.mainloop()
+
